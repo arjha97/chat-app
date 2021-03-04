09854b87edb54e360ac6a6038675b15f428701ed
Abhishek ranjan <arjha97@gmail.com>
2021-03-04 16:29:39 +0530

    added signin page with facebook and google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
     apiKey: "AIzaSyBXyGjsqOVtuk2O0JE0D_uj-UDSac8B1Kg",
@@ -11,5 +13,7 @@ const config = {
   };
 
   const app = firebase.initializeApp(config);
+  export const auth = app.auth();
+  export const database = app.database();
 
   
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React from 'react';
+import firebase from 'firebase/app';
+import { Button, Container, Grid, Icon, Panel, Col, Row, Alert } from 'rsuite'
+import { auth, database } from '../misc/firebase'
 
 const SignIn = () => {
+
+   const signInWithProvider = async (provider) => {
+
+    try {
+        const {additionalUserInfo, user} = await auth.signInWithPopup(provider);
+
+        if(additionalUserInfo.isNewUser){
+            await database.ref(`/profiles/${user.uid}`).set({
+                name: user.displayName,
+                createdAt: firebase.database.ServerValue.TIMESTAMP
+            })
+        }
+
+        Alert.success('Signed In');
+    }
+     catch (err) {
+        Alert.error(err.message, 3000)
+    }
+
+   } 
+
+   const onFacebookSignIn = () => {
+      signInWithProvider(new firebase.auth.FacebookAuthProvider()) 
+   };
+
+   const onGoogleSignIn = () => {
+      signInWithProvider(new firebase.auth.GoogleAuthProvider()) 
+   };
+
+
     return (
-        <div>
-            saf
-        </div>
+        <Container>
+            <Grid className="mt-page">
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                     <Panel>
+                         <div className="text-center">
+                             <h2>Welcome to Chat App</h2>
+                             <p>Progressive chat platform for neophytes</p>
+                         </div>
+
+                         <div className='mt-3'>
+                             <Button block color='green' onClick={onFacebookSignIn}>
+                                 <Icon icon='facebook'>Continue with Facebook</Icon>
+                             </Button>
+
+                             <Button block color='cyan' onClick={onGoogleSignIn}>
+                                 <Icon icon='google'>Continue with Google</Icon>
+                             </Button>
+                         </div>
+                     </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+        </Container>
     )
 }
 
