94620103ad55c3006a4ee8730a0fd95fcda63e99
Abhishek ranjan <arjha97@gmail.com>
2021-03-04 18:26:42 +0530

    manage user with profile context

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import  './styles/main.scss';
 
 const App = () => {
     return (
+    <ProfileProvider> 
         <Switch>
             <PublicRoute path='/signin'>
                 <SignIn/>
@@ -18,6 +20,7 @@ const App = () => {
               <Home/>   
             </PrivateRoute> 
         </Switch>
+    </ProfileProvider> 
     )
 }
 
