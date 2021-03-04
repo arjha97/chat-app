9b880618d0c3960c9c601767d13d79da658e0a0d
Abhishek ranjan <arjha97@gmail.com>
2021-03-04 22:21:54 +0530

    updated nickname

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { Button, Drawer } from 'rsuite'
+import { Alert, Button, Divider, Drawer } from 'rsuite'
 import {  useProfile } from '../../context/profile.context'
+import { database } from '../../misc/firebase';
+import EditableInput from './EditableInput';
 
 const Dashboard = ({onSignOut}) => {
 
     const {profile} = useProfile();
+
+    const onSave = async newData => {
+     const userNickname =  database.ref(`/profiles/${profile.uid}`).child('name');
+
+     try {
+         await userNickname.set(newData);
+         Alert.success('Nickname has been changed', 4000)
+     } catch (err) {
+         Alert.error(err.message, 3000);
+     }
+    }
 
     return (
        <>
@@ -16,6 +29,13 @@ const Dashboard = ({onSignOut}) => {
 
         <Drawer.Body>
           <h3>Hey, {profile.name.toUpperCase()}</h3>
+          <Divider />
+          <EditableInput
+            name="nickname"
+            initialValue ={profile.name}
+            onSave={onSave}
+            label={<h6 className="mb-2">Nickname</h6>}
+           />
         </Drawer.Body>
 
         <Drawer.Footer>
