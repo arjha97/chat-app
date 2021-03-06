de198335ea4b79172fd5d57b9fab65f84db1f746
Abhishek ranjan <arjha97@gmail.com>
2021-03-06 22:33:30 +0530

    created chat bottom and added functionality to it

--- src/components/chatWindow/bottom/index.js
+++ src/components/chatWindow/bottom/index.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react';
+import { useParams } from 'react-router';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import firebase from 'firebase/app';
+
+import { useProfile } from '../../../context/profile.context';
+
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId){
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? {avatar: profile.avatar} : {})
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP
+  }
+}
 
 const Bottom = () => {
+
+  const [input, setInput] = useState('');
+
+  const {chatId} = useParams();
+  const { profile } = useProfile();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onInputChange = useCallback((value) => {
+     setInput(value);
+  }, []);
+
+  const onSendClick = async () => {
+   if(input.trim() === ''){
+     return ;
+   }
+
+   const messageData = assembleMessage(profile, chatId);
+   messageData.text = input;
+
+   const updates = {};
+
+   const messageId = database.ref('messages').push().key;
+
+   updates[`/messages/${messageId}`] = messageData;
+   updates[`/rooms/${chatId}/lastMessage`] = {
+     ...messageData,
+     messageData: messageId
+   };
+   
+   setIsLoading(true);
+   try {
+     await database.ref().update(updates);
+     setInput('');
+     setIsLoading(false);
+
+     Alert.info('Message has been sent', 3000);
+    
+   } catch (err) {
+     setIsLoading(false);
+     Alert.error(err.message, 3000)
+   }
+
+  }
+
+  const onKeyDown = (e) => {
+    if(e.keyCode === 13) {
+      e.preventDefault();
+      onSendClick();
+    }
+  }
+
     return (
         <div>
-          n  
+          <InputGroup>
+           <Input placeholder="Write a new msg here..." value={input} onChange={onInputChange} onKeyDown={onKeyDown} />
+
+           <InputGroup.Button color="blue" appearance="primary" onClick={onSendClick} disabled={isLoading}>
+             <Icon icon="send" />
+           </InputGroup.Button>
+          </InputGroup> 
         </div>
     )
 }
